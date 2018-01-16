304beb22bf5ba9270d456708e43afef101acf1f5
xiaohuihui <2315870441@qq.com>
2018-01-17 00:30:42 +0800

    添加启动器activity，home键没反应

--- NULLauncher/app/src/main/java/com/xhh/launcher/custom/activity/app/MainActivity.java
+++ NULLauncher/app/src/main/java/com/xhh/launcher/custom/activity/app/MainActivity.java
@@ -1,4 +1,4 @@
-package com.xhh.launcher.custom;
+package com.xhh.launcher.custom.activity.app;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.xhh.launcher.custom.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
---
+++ NULLauncher/app/src/main/java/com/xhh/launcher/custom/activity/launcher/LauncherActivity.java
@@ -0,0 +1,15 @@
+package com.xhh.launcher.custom.activity.launcher;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.xhh.launcher.custom.R;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
